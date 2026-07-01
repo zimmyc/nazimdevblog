08c4177b43098e08c4625f068a23fa926c9467eb
zimmyc <nazimchoudry786@gmail.com>
2026-07-01 16:17:01 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://nazimdev.blog',
+	base: '/nazimdevblog/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
